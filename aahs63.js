64ef74f9b94dc3e3c012a72e453da7d33c494f2f
George <georgecrane@mac.com>
2021-10-09 14:59:57 -0400

    Update aahs63.js

--- aahs63.js
+++ aahs63.js
@@ -51,6 +51,8 @@ function get_spreadsheet(theurl) {
         }
         //alert("Status: " + textStatus); 
         alert("Error: " + errorThrown + " try refreshing the page."); 
+        console.log('Error: + errorThrown');
+        console.log(theurl);
       } 
   });
   return result;
